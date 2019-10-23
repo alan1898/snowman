fc95818b88be5a16e1eeaf311d92477257cd1e14
alan1898 <iszhikang@gmail.com>
2019-10-24 00:14:24 +0800

    division

--- schemes/schemes.h
+++ schemes/schemes.h
@@ -9,32 +9,4 @@
 #include "RW13.h"
 #include "BSW07.h"
 
-#include "SR01.h"
-#include "SR02.h"
-#include "SAR.h"
-
-#include "BCET.h"
-#include "BCET_test.h"
-
-#include "HCET.h"
-#include "HCET_test.h"
-
-#include "OCET.h"
-#include "OCET_test.h"
-
-#include "OHCET.h"
-#include "OHCET_test.h"
-
-#include "HABE.h"
-#include "HABE_test.h"
-
-#include "DETC.h"
-#include "DETC_test.h"
-
-#include "DET.h"
-#include "DET_test.h"
-
-#include "ODETC.h"
-#include "ODETC_test.h"
-
 #endif //ABELIB_SCHEMES_H
